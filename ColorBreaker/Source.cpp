cbb99b92861c427d9c54baa1b7579716a99e32b8
Ydot12344 <andreizdor@yandex.ru>
2022-04-07 23:39:25 +0300

    upd

--- ColorBreaker/Source.cpp
+++ ColorBreaker/Source.cpp
@@ -1,3 +1,18 @@
-#include <iostream>
+#define GLEW_STATIC
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+#include<utility>
+#include<iostream>
+#include "Window.h"
+Window w(800, 600);
 
-#include <imgay.h>
+int main() {
+
+	//init();
+
+	while (!glfwWindowShouldClose(w.getWindow())) {
+		
+	}
+
+	glfwTerminate();
+}
---
+++ ColorBreaker/Window.h
@@ -0,0 +1,41 @@
+#pragma once
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+
+class Window {
+public:
+	size_t height, width;
+	Window(size_t w, size_t h) : width(w), height(h) {
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+		glfwWindowHint(GLFW_SAMPLES, 4);
+		glEnable(GL_MULTISAMPLE);
+		window = glfwCreateWindow(width, height, "Flappy Bird", nullptr, nullptr);
+		glfwMakeContextCurrent(window);
+
+		// Setup cursore
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+		if (glfwRawMouseMotionSupported()) // for smooth movement of cursor
+			glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+
+		glewExperimental = GL_TRUE;
+		glewInit();
+
+		glViewport(0, 0, width, height);
+
+	}
+	GLFWwindow* getWindow() {
+		return window;
+	}
+
+	void setViewPort(size_t w, size_t h) {
+		glViewport(0, 0, w, h);
+	}
+
+private:
+	GLFWwindow* window = nullptr;
+
+};
